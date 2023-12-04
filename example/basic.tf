ea8333ef1230106b2d3c4b55f9cd747a15466772
Rodrigo Torres <rlinstorres@gmail.com>
2023-12-04 13:28:32 +0100

    feat: Setting cluster_orientation parameter

--- example/basic.tf
+++ example/basic.tf
@@ -1,17 +1,17 @@
 #### Create Ocean ECS Cluster ####
 module "ocean_ecs" {
-    source = "spotinst/ecs-ocean/spotinst"
+  source = "spotinst/ecs-ocean/spotinst"
 
-    cluster_name                    = "ECS-Workshop"
-    region                          = "us-west-2"
-    subnet_ids                      = ["subnet-123456789,subnet-123456789"]
-    security_group_ids              = ["sg-123456789"]
-    image_id                        = "ami-123456789"
-    iam_instance_profile            = "arn:aws:iam::123456789:instance-profile/ecsInstanceRole"
+  cluster_name         = "ECS-Workshop"
+  region               = "us-west-2"
+  subnet_ids           = ["subnet-123456789,subnet-123456789"]
+  security_group_ids   = ["sg-123456789"]
+  image_id             = "ami-123456789"
+  iam_instance_profile = "arn:aws:iam::123456789:instance-profile/ecsInstanceRole"
 
-    tags = {CreatedBy = "terraform"}
+  tags = { CreatedBy = "terraform" }
 }
 
 output "ocean_id" {
-    value = module.ocean_ecs.ocean_id
+  value = module.ocean_ecs.ocean_id
 }
